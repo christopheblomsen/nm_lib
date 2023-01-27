a779ec0674bb572106524d64129e66b05f9157ef
christopheblomsen <christophe.blomsen@gmail.com>
2023-01-27 14:29:25 +0100

    [UPDATE] minor update

--- nm_lib/test/test_ex_2b.py
+++ nm_lib/test/test_ex_2b.py
@@ -1,33 +1,66 @@
 def test_ex_2b():
     """
-    Test to see if the numerical `evolv_adv_burgers` method
-    gives a good enough approximation with a tolerance of 
-    1e-5
+    Test to see if the numerical `evolv_adv_burgers` method gives a good enough
+    approximation with a tolerance of 1e-5.
     """
+    import numpy as np
+
     from nm_lib import nm_lib as nm
-    import numpy as np 
-    
-    def u_init(x): 
-        return np.cos(6*np.pi*x/5)**2 / np.cosh(5*x**2)
-    
-    def u(x, t, a=-1): 
-        return u_init(x - a*t)
+
+    def u_init(x):
+        """
+        The initial function at t=t0.
+
+        Parameters
+        ----------
+        x   :   `array`
+             Spatial axis
+
+        Returns
+        -------
+        `array`
+            The initial function at t=t0
+        """
+        return np.cos(6 * np.pi * x / 5) ** 2 / np.cosh(5 * x**2)
+
+    def u(x, t, a=-1):
+        """
+        Analytical solution to the equation.
+
+        Parameters
+        -----------
+        x   :   `array`
+             Spatial axis
+        t   :   `array`
+             Time axis
+        a : `float` or `array`
+            Either constant, or array which multiply the right hand side of the Burger's eq.
+
+        Returns
+        -------
+        `array`
+            Analytical solution to burgers equations
+        """
+        return u_init(x - a * t)
 
     x0 = -2.6
-    xf = -1*x0
+    xf = -1 * x0
     nump = 65
-    xx = np.arange(nump)/(nump - 1) * (xf - x0) + x0
+    xx = np.arange(nump) / (nump - 1) * (xf - x0) + x0
 
-    nt = 100 
+    nt = 100
 
     U0 = u_init(xx)
     t, calculated = nm.evolv_adv_burgers(xx, U0, nt, a=1)
-    
-    expected = u(np.repeat(xx[:, np.newaxis], nt, axis=1),
-                np.repeat(t[np.newaxis, :], nump, axis=0), a=1)
 
-    tol = np.ones(len(xx))*1e-5
+    expected = u(
+        np.repeat(xx[:, np.newaxis], nt, axis=1),
+        np.repeat(t[np.newaxis, :], nump, axis=0),
+        a=1,
+    )
 
-    success = np.any(np.abs(calculated[:, 0] - expected[:, 0]) < tol)
-
-    assert success
+    tol = np.ones(len(t)) * 1e-10
+    diff = np.abs(calculated - expected)
+    success = np.any(diff < tol)
+    msg = np.max(diff)
+    assert success, msg
